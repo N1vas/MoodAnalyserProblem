7c259b60e284bca3384239d1ae6ca6a3dc69e821
N1vas <ggj.nivas.18@gmail.com>
2020-10-11 19:18:10 +0530

    [N1vas] Add:UC1 analyse happy or sad

---
+++ MoodAnalyseTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodeAnalyseAndExceptional;
+namespace MoodAnalyseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataRow("I am in Sad Mood")]
+        [DataRow("I am in Happy Mood")]
+        public void TestMethod1(string message)
+        {
+            //Arrange
+            string expected = "SAD";
+            MoodAnalyse mood = new MoodAnalyse(message);
+            //Act
+            var result = mood.analyseMood();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
